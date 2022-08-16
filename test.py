9e5a891157a3096a8a0c86c484d1eba5d3fba521
Lauuud <lho935579578@gmail.com>
2022-08-16 15:45:15 +0800

    vers

--- test.py
+++ test.py
@@ -3,3 +3,15 @@ print(a)
 
 
 bb = "这里加多一个branch"
+c = "like"
+
+my_dict = {"spam": "eggs", "foo": 4, 100: "bar", 2: 0.5, 2: 0.5, 2: 0.5, 2: 0.5, 2: 0.5}
+
+
+def ss(a: int):
+    print(a)
+
+    print("sdf")
+    print("sdf")
+
+ss("sdf")
