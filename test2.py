9973626b96ffe4aca8657916e2e400046c175ace
Lauuud <lho935579578@gmail.com>
2022-08-17 14:25:00 +0800

    Merge remote-tracking branch 'origin/main' into main

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
 a = "it is empty"
 b = "xinde commit"
 c = "改变这里"
+
+d = "测试stash"
--- testforpython.py
+++ testforpython.py
@@ -1,4 +1,5 @@
 a = "it is an test"
+<<<<<<< HEAD
 b   =    "new change"
 
 
@@ -6,3 +7,12 @@ b   =    "new change"
 
 d = "local 这里改变了"
 
+=======
+b = “ new change”
+
+def whatisit():
+  print(a)
+c = "有时一个变化”"
+>>>>>>> origin/main
+
+d = "不知道是不是这里变化"
